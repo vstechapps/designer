8f8ae7b1f8ea46a01df82bee7a08024f67901a15
vvskchaitanya <vvskchaitanya@gmail.com>
2023-01-11 20:53:04 +0000

    Added head and tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
